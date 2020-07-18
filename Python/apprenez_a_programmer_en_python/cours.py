d1b797819fa4d35f95b1a455ac480dac3df4ae7d
Axel Pion <properpina@naver.com>
2020-07-18 14:31:43 +0200

    07/18/20 + TP3 consignes

--- Python/apprenez_a_programmer_en_python/cours.py
+++ Python/apprenez_a_programmer_en_python/cours.py
@@ -495,7 +495,7 @@ fontion(1)
 
 # Autre exemple
 
-def fonction(nom, prenom, *parametre)
+def fonction(nom, prenom, *parametre):
 
 # Transformer une liste en paramêtre de fonctions
 
@@ -1882,3 +1882,145 @@ class ErreurAnalyseFichier(Exception):
         File "<stdin>", line 1, in <module>
     StopIteration
   """
+
+###
+
+class RevStr(str):
+    """
+        Classe reprenant les méthodes et attributs des chaînes construites
+        depuis 'str'. On se contente de définir une méthode de parcours
+        différente: au lieu de parcourir la chaîne de la première à la
+        dernière lettre, on la parcourt de la dernière à la première
+
+        Les autres méthodes, y compris le constructeur, n'ont pas besoin
+        d'être redéfinies
+    """
+
+        def __iter__(self):
+            """
+                Cette méthode renvoie un itérateur parcourant la chaîne
+                dan le sens inverse de celui d'un 'str'
+            """
+
+            return ItRevStr(str) # On renvoie l'itérateur créé pour l'occasion
+
+class ItRevStr:
+    """
+        Un itérateur permettant de parcourir un chaîne de la dernière lettre
+        à la première. On stocke dans des attributs la position courante et la
+        chaîne à parcourir
+    """
+
+    def __init__(self, chaine_a_parcourir):
+        """
+            On se positionne à la fin de la chaîne
+        """
+        self.chaine_a_parcourir = chaine_a_parcourir
+        self.position = len(chaine_a_parcourir)
+
+    def __next__(self):
+        """
+            Cette méthode doit renvoyer l'élément zsuivant dans le parcours,
+            ou lever l'exception 'StopIteration' si le parcours est fini
+        """
+
+        if self.position == 0: # Fin du parcours
+            raise StopIteration
+        self.position -= 1 # On décrémente la position
+        return self.chaine_a_parcourir[self.position]
+
+>>> ma_chaine = RevStr("Bonjour")
+>>> ma_chaine
+> 'Bonjour'
+>>> for lettre in ma_chaine:
+...     print(lettre)
+> 'r'
+> 'u'
+> 'o'
+> 'j'
+> 'n'
+> 'o'
+> 'B'
+
+# Les générateurs
+
+# Générateurs simples : yield
+
+def mon_generateur():
+    """
+        Notre premier générateur.
+        Il va simplement renvoyer 1, 2 et 3
+    """
+    yield 1
+    yield 2
+    yield 3
+
+>>> mon_generateur
+> '<function mon_generateur at 0x00B494F8>'
+>>> mon_generateur()
+> '<generator object mon_generateur at 0x00B9C88>'
+>>> mon_iterateur = iter(mon_generateur())
+>>> next(mon_iterateur)
+> 1
+>>> next(mon_iterateur)
+> 2
+>>> next(mon_iterateur)
+> 3
+
+# ==
+for nombre in mon_generateur():
+    print(nombre)
+
+###
+for nombre in intervalle(5, 10):
+        print(nombre)
+
+> 6
+> 7
+> 8
+> 9
+
+# ==
+
+def intervalle(borne_inf, borne_sup):
+    """
+        Générateur parcourant la série des entiers entrer borne_inf et borne_sup
+    """
+
+    borne_inf += 1
+    while borne_inf < borne_sup:
+        yied borne_inf
+        borne_inf += 1
+
+# Co-routine
+
+# Interruption de la boucle
+
+generateur = intervalle(5, 20)
+for nombre in generateur:
+    if nombre > 17:
+        generateur.close() # Interrution de la boucle
+
+# Envoyer des données à un générateur
+
+def intervalle(borne_inf, borne_sup):
+    """
+        Générateur parcourant la série des entiers entre borne_inf et borne_sup.
+        Notre générateur doit pouvoir "sauter" une certaine plage de nombres
+        en fonction d'une valeur qu'on lui donne pendant le parcours.
+        La valeur qu'on lui passe est la nouvelle valeur de borne_inf.
+    """
+
+    borne_inf += 1
+    while borne_inf < borne_sup:
+        valeur_recue = (yield borne_inf)
+        if valeur_recue is not None: # Notre générateur à reçu quelque chose
+            borne_inf = valeur_recue
+        borne_inf += 1
+
+
+generateur = intervalle(10, 25)
+for nombre in generateur:
+    if nombre == 15: # On saute à 20
+        generateur.send(20)
+    print(nombre, end=" ")
